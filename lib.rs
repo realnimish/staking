fe7103d8250cbb8341b05870a5a254aa48a1227b
realnimish <realnimish@gmail.com>
2022-03-02 02:54:37 +0530

    Single instance staking

--- lib.rs
+++ lib.rs
@@ -16,12 +16,47 @@ mod staking {
     type Time = u64;
 
     #[derive(PackedLayout, SpreadLayout)]
-    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[derive(Debug, PartialEq, Eq)]
+    #[derive(scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub struct Lock {
         locked_amt: Balance,
         locked_on: Time,
         last_claimed: Option<Time>,
+    }
+
+    impl Lock {
+
+        // Calculates the unlocked amount at a given time
+        pub fn claimable_value(&self, time: Option<Time>) -> Balance {
+            if time.is_none() {
+                return 0;
+            }
+            let time = time.unwrap();
+            let mut period = self.locked_on;
+            let mut tokens = self.locked_amt;
+            let mut claim = tokens/2;   // 50% unlocks on Day 1
+            let daily_unlock = tokens/10; // 10% unlocks daily from Day 2
+            const DAY: Time = 60*60*24;
+
+            period += DAY;
+            while time >= period && tokens > 0 {
+                tokens -= daily_unlock;
+                claim += daily_unlock;
+                if tokens < daily_unlock {
+                    // Considers the case when some tokens are left out because of division
+                    claim += tokens;
+                    tokens = 0;
+                }
+                period += DAY;
+            }
+            return claim;
+        }
+
+        // Returns the total amount locked by the given user
+        pub fn locked_amt(&self) -> Balance {
+            self.locked_amt
+        }
     }
 
     #[ink(storage)]
@@ -37,72 +72,54 @@ mod staking {
             ink_lang::utils::initialize_contract(|_| {})
         }
 
-        fn claimable_value(lock: &Lock, time: Option<Time>) -> Balance {
-            if time.is_none() {
-                return 0;
-            }
-            let time = time.unwrap();
-            let mut period = lock.locked_on;
-            let mut tokens = lock.locked_amt;
-            let mut claim = tokens/2;
-            let daily_unlock = tokens/10;
-            const DAY: Time = 60*60*24;
-
-            period += DAY;
-            while time >= period && tokens > 0 {
-                tokens -= daily_unlock;
-                claim += daily_unlock;
-                if tokens < daily_unlock {
-                    claim += tokens;
-                    tokens = 0;
-                }
-                period += DAY;
-            }
-            return claim;
-        }
-
+        /// Reads lock state of the invoker
         #[ink(message)]
         pub fn get_lock_details(&self) -> Option<Lock> {
             let caller = self.env().caller();
             self.stakes.get(&caller)
         }
 
+        /// Returns the amount of tokens still locked
         #[ink(message)]
         pub fn get_locked_amt(&self) -> Balance {
             let lock = self.get_lock_details();
             let now = self.env().block_timestamp();
             match lock {
                 None => 0,
-                Some(lock) => lock.locked_amt - Self::claimable_value(&lock, Some(now))
+                Some(lock) => lock.locked_amt() - lock.claimable_value(Some(now))
             }
         }
 
+        /// Returns the amount already claimed by the user so far
         #[ink(message)]
         pub fn get_claimed_amt(&self) -> Balance {
             let lock = self.get_lock_details();
             match lock {
                 None => 0,
-                Some(lock) => Self::claimable_value(&lock, lock.last_claimed)
+                Some(lock) => lock.claimable_value(lock.last_claimed)
             }
         }
 
+        /// Returns the amount of token that the invoker can claim at time t
         #[ink(message)]
         pub fn get_pending_amt(&self) -> Balance {
             let lock = self.get_lock_details();
             let now = self.env().block_timestamp();
             match lock {
                 None => 0,
-                Some(lock) => Self::claimable_value(&lock, Some(now)) - Self::claimable_value(&lock, lock.last_claimed)
+                Some(lock) => lock.claimable_value(Some(now)) - lock.claimable_value(lock.last_claimed)
             }
         }
 
-        #[ink(message)]
-        pub fn insert(&mut self, locked_amt: Balance, locked_on: Time) {
+        // @dev: Only there for simple testing
+        fn insert(&mut self, locked_amt: Balance, locked_on: Time) {
             let caller = self.env().caller();
             let lock = Lock{locked_amt, locked_on, last_claimed:None};
             self.stakes.insert(&caller,&lock);
         }
 
+        /// User can send tokens for staking
+        /// User can stake only when no previous stake order is pending
         #[ink(message,payable)]
         pub fn lock_tokens(&mut self) {
             let locked_amt = self.env().transferred_value();
@@ -118,6 +135,7 @@ mod staking {
             self.stakes.insert(&caller,&lock);
         }
 
+        /// User can claim their pending tokens which have been unlocked
         #[ink(message)]
         pub fn claim_tokens(&mut self) {
             let value = self.get_pending_amt();
